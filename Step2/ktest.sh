45be0abe6ee79d22684ec13e3c0873dcd8307e42
151250012 <151250012@smail.nju.edu.cn>
2017-08-01 11:19:00 +0800

    change fileOperate.h
    change the conditions’s file name

--- Step2/ktest.sh
+++ Step2/ktest.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
+
+clang++-3.4 main.cc
+./a.out
+
+clang -I ../../include -emit-llvm -O0 -c -g example.c -o example.bc
+klee example.bc
+
 : > ./result.txt #clear the result.txt
 for ktest_file in `ls ./klee-last`
 do
 	ktest-tool --write-floats klee-last/$ktest_file
 done
 
+cd ./checking
+rm -rf *
+cd ..
+
+clang++-3.4 analyze.cc
+./a.out
+
 for checking_file in `ls ./checking`
 do
 	clang++-3.4 ./checking/$checking_file
-	./a.out
+	./checking/a.out
 done
